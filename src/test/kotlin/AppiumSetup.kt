0a938193231551034394c1f6b20f120880723781
John Doe <benjamin.work@hotmail.fr>
2022-06-13 10:38:36 +0200

    Update copyright

--- src/test/kotlin/AppiumSetup.kt
+++ src/test/kotlin/AppiumSetup.kt
@@ -1,4 +1,16 @@
-
+/*
+ * Copyright (C) 2021 Baracoda Daily Healthtech
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
@@ -12,14 +24,15 @@ import java.net.URL
 open class AppiumSetup {
 
     companion object {
-        lateinit var driver: AppiumDriver<MobileElement>
-        private val caps = DesiredCapabilities()
+
         private const val activityName = "com.android.calculator2.Calculator"
         private const val appPackage = "com.google.android.calculator"
         private const val automationName = "UiAutomator2"
         private const val platformName = "Android"
         private const val serverUrl = "http://localhost:4723/wd/hub"
 
+        lateinit var driver: AppiumDriver<MobileElement>
+        private val caps = DesiredCapabilities()
         @JvmStatic
         @BeforeAll
         fun setUp() {
